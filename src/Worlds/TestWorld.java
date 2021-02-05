ca20e99166b4ccfd287ad59bbdfcf27e57eba7ae
Roniton <henrikstarke@yahoo.de>
2021-02-05 19:37:33 +0100

    Engine

---
+++ src/Worlds/TestWorld.java
@@ -0,0 +1,33 @@
+package Worlds;
+
+import Main.Game;
+
+import java.awt.*;
+
+public class TestWorld extends Worlds {
+
+
+    /**
+     * Constructor
+     *
+     * @param game Main.Main.Game Object
+     */
+    public TestWorld(Game game) {
+        super(game);
+    }
+
+    /**
+     * updates the world
+     */
+    public void tick() {
+    }
+
+    /**
+     * renders the world
+     *
+     * @param g -> Graphics g
+     */
+    public void render(Graphics g) {
+    }
+
+}
---
+++ src/Worlds/Worlds.java
@@ -0,0 +1,50 @@
+package Worlds;
+
+import Main.Game;
+
+import java.awt.*;
+
+public abstract class Worlds {
+
+    protected Game game;
+    private static Worlds currentWorld = null;
+
+    /**
+     * Constructor
+     *
+     * @param game
+     */
+    public Worlds(Game game) {
+        this.game = game;
+    }
+
+    /**
+     * sets a new world
+     *
+     * @param world -> new world
+     */
+    public static void setWorld(Worlds world) {
+        currentWorld = world;
+    }
+
+    /**
+     * @return current world
+     */
+    public static Worlds getWorld() {
+        return currentWorld;
+    }
+
+    /**
+     * updates the world
+     */
+    public abstract void tick();
+
+
+
+    /**
+     * renders the world
+     *
+     * @param g Graphics g
+     */
+    public abstract void render(Graphics g);
+}
