1efdcc0ee2ed9107c649d012c7b9911a9bbd793f
Roniton <henrikstarke@yahoo.de>
2021-02-05 23:13:17 +0100

    new init

---
+++ src/Main/ImageLoader.java
@@ -0,0 +1,22 @@
+package Main;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ImageLoader {
+
+    /**
+     * Loads an Image
+     * @param path Path of the Image
+     */
+    public static BufferedImage loadImage(String path){
+        try {
+            return ImageIO.read(ImageLoader.class.getResource(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+}
